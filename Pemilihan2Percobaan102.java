1da7f58946733f57e5729775709c45adb76257e9
Yuanzth <adityayuhanda3001@gmail.com>
2023-10-05 11:43:28 +0700

    Percobaan 1 Pemilihan 2 #2

--- Pemilihan2Percobaan102.java
+++ Pemilihan2Percobaan102.java
@@ -15,10 +15,9 @@ if ((tahun % 4) == 0)
 {
     if ((tahun % 100) != 0)
         System.out.println("Tahun Kabisat");
+    else 
+        System.out.println("Bukan Tahun Kabisat");
 }
-else 
-    System.out.println("Bukan Tahun Kabisat");
-
 }
     
 }
