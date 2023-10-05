ea86160767787848e69af282d106c26088befbef
Yuanzth <adityayuhanda3001@gmail.com>
2023-10-05 12:05:05 +0700

    Percobaan 1 Pemilihan 2 #3

--- Pemilihan2Percobaan102.java
+++ Pemilihan2Percobaan102.java
@@ -1,23 +1,28 @@
 import java.util.Scanner;
-
 public class Pemilihan2Percobaan102 {
-
 public static void main(String[] args) {
-    
     Scanner input02 = new Scanner(System.in);
-
 int tahun;
-
 System.out.println("Masukan Tahun : ");
 tahun = input02.nextInt();
 
 if ((tahun % 4) == 0) 
 {
     if ((tahun % 100) != 0)
-        System.out.println("Tahun Kabisat");
+    {
+        if ((tahun % 400) != 0)
+        {
+            System.out.println("Tahun Kabisat");
+        }
+        else 
+        {
+            System.out.println("Bukan Tahun Kabisat");
+        } 
+    }
     else 
+    {
         System.out.println("Bukan Tahun Kabisat");
+    }
 }
 }
-    
 }
